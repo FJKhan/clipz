8de26113a5d44778af6e4dad11d7469b2489fe45
FJKhan <fjgenus@gmail.com>
2025-12-26 14:56:11 -0500

    refactor: :recycle: refactor auth modal to use tabs and tab container components

--- src/app/user/auth-modal/auth-modal.ts
+++ src/app/user/auth-modal/auth-modal.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Modal } from '../../shared/modal/modal';
+import { TabsContainer } from '../../shared/tabs-container/tabs-container';
+import { Tab } from '../../shared/tab/tab';
 
 @Component({
   selector: 'app-auth-modal',
-  imports: [Modal],
+  imports: [Modal, Tab, TabsContainer],
   templateUrl: './auth-modal.html',
-  styleUrl: './auth-modal.css'
+  styleUrl: './auth-modal.css',
 })
-export class AuthModal {
-
-}
+export class AuthModal {}
