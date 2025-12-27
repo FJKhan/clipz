d1958014d65b0aa22daf47d87ecb2ac16d7e1ebe
FJKhan <fjgenus@gmail.com>
2025-12-27 15:49:47 -0500

    refactor: :recycle: refactor register component to use input component

--- src/app/user/register/register.ts
+++ src/app/user/register/register.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators, ValidationErrors } from '@angular/forms';
+import { Input } from "../../shared/forms/input/input";
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, Input],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
+  formBuilder = inject(FormBuilder);
+  form = this.formBuilder.nonNullable.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    age: [18, [Validators.required, Validators.min(18)]],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
+      ],
+    ],
+    confirmPassword: ['', [Validators.required]],
+    phoneNumber: ['', [Validators.required, Validators.minLength(12), Validators.maxLength(12)]],
+  });
 
+  register() {
+    
+  }
 }
