3d6249aa0995236942152d4b7a8fc5b12d1c0750
FJKhan <fjgenus@gmail.com>
2025-12-29 17:28:45 -0500

    feat: :sparkles: add alert to register modal

--- src/app/user/register/register.ts
+++ src/app/user/register/register.ts
@@ -1,14 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators, ValidationErrors } from '@angular/forms';
-import { Input } from "../../shared/forms/input/input";
+import { Input } from '../../shared/forms/input/input';
+import { Alert } from '../../shared/alert/alert';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, Input],
+  imports: [ReactiveFormsModule, Input, Alert],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
 export class Register {
+  alertColour = signal('blue');
+  showAlert = signal(false);
+  alertMsg = signal('Please wait while your account is being created.');
+
   formBuilder = inject(FormBuilder);
   form = this.formBuilder.nonNullable.group({
     name: ['', [Validators.required, Validators.minLength(2)]],
@@ -27,6 +32,13 @@ export class Register {
   });
 
   register() {
-    
+    /**todo: register logic */
+    this.alertColour.set('indigo');
+    this.showAlert.set(true);
+  }
+
+  resetAlert() {
+    this.alertColour.set('blue');
+    this.alertMsg.set('Please wait while your account is being created.');
   }
 }
